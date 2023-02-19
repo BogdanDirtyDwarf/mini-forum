4cb86d214f34a30707f7fee7dec59de3bd7329aa
BogdanDirtyDwarf <i21s045@students.it-college.ru>
2023-02-19 17:17:13 +0500

    [TECH] Added devServer & env

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,4 +1,5 @@
 import webpack from "webpack";
+import { buildDevServer } from "./buildDevServer";
 import { buildLoaders } from "./buildLoaders";
 import { buildPlugins } from "./buildPlugins";
 import { buildResolvers } from "./buildResolvers";
@@ -6,7 +7,7 @@ import { BuildOptions } from "./types/config";
 export function buildWebpackConfig(
   options: BuildOptions
 ): webpack.Configuration {
-    const {paths, mode} = options
+    const {paths, mode, isDev} = options
   return {
     mode: mode,
     entry: paths.entry,
@@ -19,6 +20,8 @@ export function buildWebpackConfig(
       path: paths.build,
       clean: true,
     },
+    devtool: isDev ? "inline-source-map" : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
     plugins: buildPlugins(options),
   };
 }
