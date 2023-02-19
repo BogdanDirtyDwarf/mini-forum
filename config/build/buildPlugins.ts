ba03b7ffb1cbe115947cc1a192f73ae707f3412c
BogdanDirtyDwarf <i21s045@students.it-college.ru>
2023-02-19 15:18:28 +0500

    [TECH] Decomposed webpack config

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,12 @@
+import { BuildOptions } from './types/config';
+import webpack from "webpack";
+import HTMLWebpackPlugin from "html-webpack-plugin";
+
+export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
+  return [
+    new HTMLWebpackPlugin({
+      template: paths.html,
+    }),
+    new webpack.ProgressPlugin(),
+  ];
+}
