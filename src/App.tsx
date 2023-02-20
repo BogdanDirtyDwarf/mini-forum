8404572a01da0af525b2acf186afa58e691cb84b
BogdanDirtyDwarf <i21s045@students.it-college.ru>
2023-02-20 12:30:17 +0500

    [UI] Created App Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
-import { Suspense } from "react";
-import { Link, Route, Routes } from "react-router-dom";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
+import './styles/index.scss';
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
 
-export const App: React.FunctionComponent = () => {
-  return (
-    <>
-      <Link to={"/"}>На главную</Link>
-      <Link to={"/about"}>О сайте</Link>
-      <Suspense fallback={<div>loading...</div>}>
-        <Routes>
-          <Route path={"/about"} element={<AboutPageAsync />} />
-          <Route path={"/"} element={<MainPageAsync />} />
-        </Routes>
-      </Suspense>
-    </>
-  );
+const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
+    return (
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />} />
+                </Routes>
+            </Suspense>
+        </div>
+    );
 };
+
+export default App;
