d99b66c4dde8dc6fbaea45ccf6126f54aea3149b
BogdanDirtyDwarf <i21s045@students.it-college.ru>
2023-03-09 19:37:25 +0500

    [LINT] Code linted

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -2,11 +2,11 @@ import { BuildOptions } from "./types/config";
 import webpack from "webpack";
 
 export function buildResolvers(option: BuildOptions): webpack.ResolveOptions {
-  return {
-    extensions: [".tsx", ".ts", ".js"],
-    preferAbsolute: true,
-    modules: [option.paths.src, "node_modules"],
-    mainFiles: ["index"],
-    alias: {},
-  };
+	return {
+		extensions: [".tsx", ".ts", ".js"],
+		preferAbsolute: true,
+		modules: [option.paths.src, "node_modules"],
+		mainFiles: ["index"],
+		alias: {},
+	};
 }
--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -5,23 +5,23 @@ import { buildPlugins } from "./buildPlugins";
 import { buildResolvers } from "./buildResolvers";
 import { BuildOptions } from "./types/config";
 export function buildWebpackConfig(
-  options: BuildOptions
+	options: BuildOptions
 ): webpack.Configuration {
-  const { paths, mode, isDev } = options;
-  return {
-    mode: mode,
-    entry: paths.entry,
-    module: {
-      rules: buildLoaders(options),
-    },
-    resolve: buildResolvers(options),
-    output: {
-      filename: "[name].[contenthash].js",
-      path: paths.build,
-      clean: true,
-    },
-    devtool: isDev ? "inline-source-map" : undefined,
-    devServer: isDev ? buildDevServer(options) : undefined,
-    plugins: buildPlugins(options),
-  };
+	const { paths, mode, isDev } = options;
+	return {
+		mode: mode,
+		entry: paths.entry,
+		module: {
+			rules: buildLoaders(options),
+		},
+		resolve: buildResolvers(options),
+		output: {
+			filename: "[name].[contenthash].js",
+			path: paths.build,
+			clean: true,
+		},
+		devtool: isDev ? "inline-source-map" : undefined,
+		devServer: isDev ? buildDevServer(options) : undefined,
+		plugins: buildPlugins(options),
+	};
 }
