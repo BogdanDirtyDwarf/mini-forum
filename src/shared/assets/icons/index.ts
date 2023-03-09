d44a1a7129e33ae50e6e56b13cff162e0fd8f7e3
BogdanDirtyDwarf <i21s045@students.it-college.ru>
2023-03-09 16:17:30 +0500

    [WIDGET] Added launguage switcher icon

--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -1,4 +1,5 @@
 import DarkModeIcon from "./Dark-Mode.svg";
 import LightModeIcon from "./Light-Mode.svg";
+import TranslateIcon from "./Translate.svg"
 
-export { DarkModeIcon, LightModeIcon };
+export { DarkModeIcon, LightModeIcon, TranslateIcon };
