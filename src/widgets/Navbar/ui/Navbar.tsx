823553dcc93373e1dec360162e3dc5de9ef9925d
BogdanDirtyDwarf <i21s045@students.it-college.ru>
2023-03-09 16:14:47 +0500

    [WIDGET] Added language switcher button

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,24 +1,29 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppLink } from "shared/ui/AppLink/AppLink";
 import { ThemeSwitcher } from "widgets/ThemeSwitcher";
+import { LangSwitcher } from "widgets/LangSwitcher";
 import cls from "./Navbar.module.scss";
+import { useTranslation } from "react-i18next";
 
 interface NavbarProps {
   className?: string;
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
+  const { t } = useTranslation();
+
   return (
     <div className={classNames(cls.navbar)}>
-      <div className={classNames(cls.navbar__text)}>My App</div>
+      <div className={classNames(cls.navbar__text)}>Mini Forum</div>
       <div className={classNames(cls.links)}>
         <AppLink to={"/"} className={classNames(cls.navbar__text)}>
-          Главная
+          {t("Главная")}
         </AppLink>
         <AppLink to={"/about"} className={classNames(cls.navbar__text)}>
-          О сайте
+          {t("О сайте")}
         </AppLink>
         <ThemeSwitcher />
+        <LangSwitcher />
       </div>
     </div>
   );
