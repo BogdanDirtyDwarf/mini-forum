273edf7b645e14ebba04d0612fc11f8eb0de9e2c
BogdanDirtyDwarf <i21s045@students.it-college.ru>
2023-03-09 19:08:18 +0500

    [TECH] Added babel

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -7,6 +7,26 @@ export function buildLoaders(option: BuildOptions): webpack.RuleSetRule[] {
     use: ["@svgr/webpack"],
   };
 
+  const babelLoader = {
+    test: /\.(js|jsx|ts|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+        plugins: [
+          [
+            "i18next-extract",
+            {
+              locales: ["ru", "en"],
+              keyAsDefaultValue: true,
+            },
+          ],
+        ],
+      },
+    },
+  };
+
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -36,5 +56,5 @@ export function buildLoaders(option: BuildOptions): webpack.RuleSetRule[] {
     use: "ts-loader",
     exclude: /node_modules/,
   };
-  return [fileLoader, svgLoader, typescriptLoader, cssLoader];
+  return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
 }
